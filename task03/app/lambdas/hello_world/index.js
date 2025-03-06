5c27dd44a6802e3dee1215aff6ee2b7a460f81a0
Harshil Prajapati <98202467+harshil07cric@users.noreply.github.com>
2025-03-06 10:54:53 +0530

    Update index.js

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        message: JSON.stringify('Hello from Lambda'),
+        message: JSON.('Hello from Lambda'),
     };
     return response;
 };
